ec535fe103757e2a9e3780949cd395cca83ef847
Rahul Soni <rahul.soni3883@gmail.com>
2019-12-15 14:10:48 +0530

    ...

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-// $(document).ready(function() {
+$(document).ready(function() {
 // 	$('div').each((index, item) => {
 // 	  $(item).attr('tabIndex', '0');
 //   });
@@ -12,4 +12,15 @@
 //     e.stopPropagation();
 //     $(this).attr('contentEditable', false);
 //   });
-// });
+
+  let s = '';
+
+  $('.skills > .section-content.pins > div').each((index, item) => {
+    s+=$(item).text()+', ';
+  });
+
+  s = s.slice(0, -2);
+
+  console.log(s);
+
+});
